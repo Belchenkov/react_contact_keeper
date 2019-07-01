2b15bf38557f43e277edc92e18e077181820488d
Алексей Бельченков <u608110@gmail.com>
2019-07-01 11:59:39 +0400

    Auth, Users, Contacts - Backend Routes

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const app = express();
 
 app.get('/', (req, res) => res.send('Hello'));
 
+// Define Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
