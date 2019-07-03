295e06fd1c7ea7051f70d73c66a43ad7ab28db79
Алексей Бельченков <u608110@gmail.com>
2019-07-03 12:08:34 +0400

    Hash Passwords & Register Route

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator/check');
+const bcrypt = require('bcryptjs');
+const { check, validationResult } = require('express-validator');
 
 const User = require('../models/User');
 
 // @route   POST api/users
 // @desc    Register a user
 // @access  Public
-router.post('/', [
+router.post('/',
+[
     check('name', 'Please add name')
         .not()
         .isEmpty(),
@@ -15,14 +17,38 @@ router.post('/', [
         .isEmail(),
     check('password', 'Please enter a password with 6 or more characters')
         .isLength({ min: 6 }),
-], (req, res) => {
+], async (req, res) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
 
-    res.send('passed');
+    const { name, email, password } = req.body;
+
+    try {
+        let user = await User.findOne({ email });
+
+        if (user) {
+            return res.status(400).json({ msg: 'User already exists!' });
+        }
+
+        user = new User({
+            name,
+            email,
+            password
+        });
+
+        const salt = await bcrypt.genSalt(10);
+
+        user.password = await bcrypt.hash(password, salt);
+        await  user.save();
+
+        res.send('User saved');
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error!');
+    }
 });
 
 module.exports = router;
