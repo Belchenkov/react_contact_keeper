bc3cbc2bb81d84fb8724d5a848e0ee4f9ec02a7e
Алексей Бельченков <u608110@gmail.com>
2019-07-04 12:28:33 +0400

    Contact Model & Get Contacts Route

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+
+const User = require('../models/User');
+const Contact = require('../models/Contact');
+const auth = require('../middleware/auth');
 
 // @route   GET api/contacts
 // @desc    Get all users contacts
 // @access  Private
-router.get('/', (req, res) => {
-    res.send('Get all contacts');
+router.get('/', auth, async (req, res) => {
+    try {
+        const contacts = await Contact.find({ user: req.user.id }).sort({ date: '-1' });
+        res.json(contacts);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error!');
+    }
 });
 
 // @route   POST api/contacts
