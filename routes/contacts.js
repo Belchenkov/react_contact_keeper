55aef316da5899b7a62ea78ac80811a57ade297b
Алексей Бельченков <u608110@gmail.com>
2019-07-05 16:35:52 +0400

    Add Contact Routes

--- routes/contacts.js
+++ routes/contacts.js
@@ -22,8 +22,31 @@ router.get('/', auth, async (req, res) => {
 // @route   POST api/contacts
 // @desc    Add new contact
 // @access  Private
-router.post('/', (req, res) => {
-    res.send('Add contact');
+router.post('/', [auth, [
+    check('name', 'Name is required.').not().isEmpty()
+]], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, phone, type } = req.body;
+
+    try {
+        const newContact = new Contact({
+            name,
+            email,
+            phone,
+            type,
+            user: req.user.id
+        });
+
+        const contact = await newContact.save();
+        res.json(contact);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error!');
+    }
 });
 
 // @route   PUT api/contacts/:id
