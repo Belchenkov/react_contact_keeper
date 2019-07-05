6a5f7f63eb83ab22b1ee04e2d7dabcd515f1feb5
Алексей Бельченков <u608110@gmail.com>
2019-07-05 17:05:59 +0400

    Contact Update

--- routes/contacts.js
+++ routes/contacts.js
@@ -52,8 +52,38 @@ router.post('/', [auth, [
 // @route   PUT api/contacts/:id
 // @desc    Update contact
 // @access  Private
-router.put('/:id', (req, res) => {
-    res.send('Update contact');
+router.put('/:id', auth, async (req, res) => {
+    const { name, email, phone, type } = req.body;
+
+    // Build contact object
+    const contactFields = {};
+
+    if (name) contactFields.name = name;
+    if (email) contactFields.email = email;
+    if (phone) contactFields.phone = phone;
+    if (type) contactFields.type = type;
+
+    try {
+        let contact = await Contact.findById(req.params.id);
+
+        if (!contact) return res.status(404).json({ msg: 'Contact not found' });
+
+        // Make sure user owns contact
+        if (contact.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: 'Not authorized' });
+        }
+
+        contact = await Contact.findByIdAndUpdate(
+            req.params.id,
+            { $set: contactFields },
+            { new: true }
+            );
+
+        res.json(contact);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error!');
+    }
 });
 
 // @route   DELETE api/contacts/:id
