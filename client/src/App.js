b60f2a1c932f09e09d630ceda6e15a07af9dd57b
Алексей Бельченков <u608110@gmail.com>
2019-07-09 11:23:10 +0400

    Contacts Context & Global State

--- client/src/App.js
+++ client/src/App.js
@@ -6,19 +6,23 @@ import Navbar from "./components/layout/Navbar";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 
+import ContactState from './context/contact/ContactState';
+
 const App = () => {
     return (
-        <Router>
-          <Fragment>
-            <Navbar />
-            <div className="container">
-              <Switch>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/about" component={About} />
-              </Switch>
-            </div>
-          </Fragment>
-        </Router>
+        <ContactState>
+          <Router>
+            <Fragment>
+              <Navbar />
+              <div className="container">
+                <Switch>
+                  <Route exact path="/" component={Home} />
+                  <Route exact path="/about" component={About} />
+                </Switch>
+              </div>
+            </Fragment>
+          </Router>
+        </ContactState>
     );
 };
 
---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,64 @@
+import React, { useReducer } from 'react';
+import uuid from 'uuid';
+
+import ContactContext from './contactContext';
+import contactReducer from './contactReducer';
+import {
+    ADD_CONTACT,
+    DELETE_CONTACT,
+    SET_CURRENT,
+    CLEAR_CURRENT,
+    UPDATE_CONTACT
+} from '../types';
+
+const ContactState = props => {
+    const initialState = {
+        contacts: [
+            {
+                id: 1,
+                name: 'Jill Johnson',
+                phone: '111-111-1111',
+                type: 'personal',
+            },
+            {
+                id: 2,
+                name: 'Sara Watson',
+                phone: '222-222-2222',
+                type: 'personal',
+            },
+            {
+                id: 3,
+                name: 'Harry White',
+                phone: '333-333-3333',
+                type: 'professional',
+            }
+        ]
+    };
+
+    const [ state, dispatch ] = useReducer(contactReducer, initialState);
+
+    // Add Contact
+
+    // Delete Contact
+
+    // Set Current Contact
+
+    // Clear Current Contact
+
+    // Update Contact
+
+    // Filter Contacts
+
+    // Clear Filter
+
+    return (
+        <ContactContext.Provider
+            value = {{
+              contacts: state.contacts
+            }}>
+            { props.children }
+        </ContactContext.Provider>
+    );
+};
+
+export default ContactState;
