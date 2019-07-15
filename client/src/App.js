74e670b3c10c4d43d5369ab93a8ed8eb7a0f3486
Алексей Бельченков <u608110@gmail.com>
2019-07-15 11:33:57 +0400

    Register Form

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import Navbar from "./components/layout/Navbar";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
+import Register from "./components/auth/Register";
 
 import ContactState from './context/contact/ContactState';
 import AuthState from './context/auth/AuthState';
@@ -20,6 +21,7 @@ const App = () => {
                     <Switch>
                       <Route exact path="/" component={Home} />
                       <Route exact path="/about" component={About} />
+                      <Route exact path="/register" component={Register} />
                     </Switch>
                   </div>
                 </Fragment>
