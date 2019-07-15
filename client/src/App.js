1d09981e19169679fffc41aad22025f7ff71ed1a
Алексей Бельченков <u608110@gmail.com>
2019-07-15 11:38:44 +0400

    Login Form

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Navbar from "./components/layout/Navbar";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
 import ContactState from './context/contact/ContactState';
 import AuthState from './context/auth/AuthState';
@@ -22,6 +23,7 @@ const App = () => {
                       <Route exact path="/" component={Home} />
                       <Route exact path="/about" component={About} />
                       <Route exact path="/register" component={Register} />
+                      <Route exact path="/login" component={Login} />
                     </Switch>
                   </div>
                 </Fragment>
