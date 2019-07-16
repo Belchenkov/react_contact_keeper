87ca650e0f47f4ae2f14e96a7b68def66c6d3d54
Алексей Бельченков <u608110@gmail.com>
2019-07-16 17:41:55 +0400

    Load User & Set Token

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,11 @@ import Alerts from "./components/layout/Alerts";
 import ContactState from './context/contact/ContactState';
 import AuthState from './context/auth/AuthState';
 import AlertState from './context/alert/AlertState';
+import setAuthToken from './utils/setAuthToken';
+
+if (localStorage.token) {
+    setAuthToken(localStorage.token);
+}
 
 const App = () => {
     return (
